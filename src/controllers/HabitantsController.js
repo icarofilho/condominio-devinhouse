7769efbeee49c40ce399427bef46e19006a25590
Icaro Ferreira <icaro.ferreira.filho@gmail.com>
2022-05-11 14:15:08 -0300

    adicionado gerenciador de logs

--- src/controllers/HabitantsController.js
+++ src/controllers/HabitantsController.js
@@ -3,6 +3,7 @@ import { removeAccents } from "../utils/transformacoes";
 import { Op } from "sequelize";
 import { mesesDoAno } from "../utils/constantes";
 import { numberCheck } from "../utils/checagem";
+import { logger } from "../config/logger";
 
 module.exports = {
   //! SOLICITADO PELO CLIENTE
@@ -12,8 +13,10 @@ module.exports = {
       const allHabitants = await Habitante.findAll({
         attributes: ["id", "nome"],
       });
+      logger.info(`Listando todos os habitantes`);
       return res.status(200).json({ habitantes: allHabitants });
     } catch (error) {
+      logger.error(error.message);
       return res.status(500).json({ error: error.message });
     }
   },
@@ -33,10 +36,17 @@ module.exports = {
         },
       });
 
-      habitante && res.status(200).json({ habitante });
-      !habitante && res.status(404).json({ error: "Nenhum habitante encontrado com ID fornecido" });
+      if (habitante) {
+        logger.info(`Listando o habitante ${habitante.nome}`);
+        return res.status(200).json({ habitante });
+      }
+
+      if (!habitante) {
+        throw new Error("Nenhum habitante encontrado com ID fornecido");
+      }
     } catch (error) {
-      return res.status(500).json({ error: error.message });
+      logger.error(`Erro na aplicação: ${error.message} - status: 400`);
+      return res.status(400).json({ error: error.message });
     }
   },
   //! SOLICITADO PELO CLIENTE
@@ -49,8 +59,15 @@ module.exports = {
           where: { nome: nome },
           attributes: ["id", "nome"],
         });
-        habitantes && res.status(200).json({ habitantes });
-        !habitantes && res.status(404).json({ error: "Nenhum habitante com o nome fornecido" });
+
+        if (habitantes) {
+          logger.info(`Listando os habitantes com nome ${habitantes.nome}`);
+          return res.status(200).json({ habitantes });
+        }
+
+        if (!habitantes) {
+          throw new Error("Nenhum habitante com o nome fornecido" );
+        }
       }
       if (sobrenome) {
         const habitantes = await Habitante.findAll({
@@ -87,6 +104,7 @@ module.exports = {
         return res.status(200).json({ habitantes: findHabitants });
       }
     } catch (error) {
+      logger.error(`${error.message} - status: 400`);
       return res.status(400).json({ error: error.message });
     }
   },
@@ -108,9 +126,10 @@ module.exports = {
         where: { data_nascimento: { [Op.lte]: born_date } },
         attributes: ["id", "nome"],
       });
-
+      logger.info(`Listando ${habitantes.length} habitantes com idade igual ou superior a ${age}`);
       return res.status(200).json({ habitantes });
     } catch (error) {
+      logger.error(error.message, " status: 400");
       return res.status(400).json({ error: error.message });
     }
   },
@@ -149,8 +168,10 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date(),
       });
+      logger.info(`Habitante ${habitante.nome} cadastrado com sucesso`);
       return res.status(201).json({ message: "Morador cadastrado com sucesso", data: habitante });
     } catch (error) {
+      logger.error(`${error.message} - status : 400`);
       return res.status(400).json({ error: error.message });
     }
   },
@@ -171,8 +192,10 @@ module.exports = {
       if (!habitante) {
         throw new Error("Nenhum morador encontrado com o id fornecido");
       }
+      logger.info(`Habitante ${habitante.nome} deletado com sucesso`);
       return res.status(200).json({ message: "Morador removido com sucesso" });
     } catch (error) {
+      logger.error(`${error.message} - status: 400`);
       return res.status(400).json({ error: error.message });
     }
   },
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "./database";
 import express from "express";
-
+import morgan from './config/morgan'
 import router from "./routes";
-
+// import morgan from 'morgan'
 const App = express();
 
 App.use(express.json());
+App.use(morgan)
 App.use(router);
 
 export default App;
